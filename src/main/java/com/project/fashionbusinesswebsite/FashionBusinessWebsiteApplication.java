0358329e0143055ee3185ea4c1c71ae326913e0f
TUANPC\tranq <nolan.tran@alpaca.vn>
2022-11-10 00:09:07 +0700

    initial project

---
+++ src/main/java/com/project/fashionbusinesswebsite/FashionBusinessWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.fashionbusinesswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionBusinessWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionBusinessWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/fashionbusinesswebsite/FashionBusinessWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fashionbusinesswebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionBusinessWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
