1ba7871baa1cec81beb4e364ab3be89486852f27
TUANPC\tranq <tranquoctuan31012001@gmail.com>
2022-11-21 17:55:41 +0700

    update data

--- sql.sql
+++ sql.sql
@@ -1,16 +1,33 @@
-Create datebase clothing_store;
+create database clothing_store;
 use clothing_store;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cart`
+--
+
 DROP TABLE IF EXISTS `cart`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cart` (
+                        `cart_id` int NOT NULL AUTO_INCREMENT,
                         `products_id` int NOT NULL,
-                        `ip_add` varchar(255) NOT NULL,
+                        `money` double NOT NULL,
                         `quantity` int NOT NULL,
-                        `size` text NOT NULL,
                         `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-                        `c_id` varchar(255) NOT NULL,
-                        PRIMARY KEY (`products_id`)
+                        `customer_id` int NOT NULL,
+                        `status` int DEFAULT NULL,
+                        PRIMARY KEY (`cart_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -104,28 +121,6 @@ INSERT INTO `discount` VALUES (1,10,1),(2,5,1);
 UNLOCK TABLES;
 
 --
--- Table structure for table `hibernate_sequence`
---
-
-DROP TABLE IF EXISTS `hibernate_sequence`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `hibernate_sequence` (
-    `next_val` bigint DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `hibernate_sequence`
---
-
-LOCK TABLES `hibernate_sequence` WRITE;
-/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (14);
-/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `orders`
 --
 
@@ -193,9 +188,6 @@ CREATE TABLE `products` (
                             `product_price` double NOT NULL,
                             `product_description` text,
                             `product_quantity` int NOT NULL,
-                            `discount_id` int NOT NULL,
-                            `discount_percent` double NOT NULL,
-                            `product_id` int NOT NULL,
                             PRIMARY KEY (`products_id`),
                             CONSTRAINT `products_chk_1` CHECK ((`product_quantity` >= 0))
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -207,7 +199,31 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,4,1,'2022-11-19 10:33:52','Men Terry Jogers','product1.jpg',145500,'Mẫu thời trang Thu - Đông đẹp nhất năm',10,0,0,0),(2,3,2,'2022-11-19 10:33:52','Wild Leg Denim','product2.jpg',177800,'Rất phong cách và dễ dàng',5,0,0,0),(3,2,2,'2022-11-19 10:33:51','Printed Days Tee','product3.jpg',80770,'Rất mát mẻ và thoải mái',20,0,0,0),(4,3,1,'2022-11-19 13:09:25','Men Stretch Skinny','product4.jpg',227700,'Phong cách và Mát mẻ',3,0,0,0),(6,2,3,'2022-11-19 10:33:51','BOYS TANK TOP','shirt_caro_msh1009.jpg',888976,'Rất hợp thời trang',21,0,0,0),(7,2,2,'2022-11-19 10:33:52','Black JUMPSUIT','product6.jpg',299909,'Rất thoải mái và mát mẻ',22,0,0,0),(9,1,3,'2022-11-19 11:11:49','Black Jacket','white_jacket.jpg',321100,'Rất ấm áp và thoải mái',24,0,0,0),(10,1,3,'2022-11-19 11:11:49','Red Parachute Jacket','yellow_jacket.jpg',23000,'Rất thoải mái và ấm áp',22,0,0,0),(11,2,2,'2022-11-19 10:33:51','Printed White Tee','short_shirt_msh1010.jpg',75000,'Rất thoải mái và ấm áp',11,0,0,0),(12,1,2,'2022-11-19 10:33:51','Brown Coat Type Jacket','productbig1.jpg',270000,'Thoải mái và ấm áp',5,0,0,0),(13,1,2,'2022-11-19 10:33:51','Pink Fluffy Jacket','productbig2.jpg',320000,'Rất thoải mái và ấm áp',6,0,0,0),(14,4,2,'2022-11-19 10:33:51','Black High Heels','productbig3.jpg',230000,'Rất phong cách và thoải mái',7,0,0,0),(15,1,1,'2022-11-19 10:33:51','Grey Royal Jacket','productbig4.jpg',350000,'Phong cách ấm áp và thoải mái',8,0,0,0),(16,4,2,'2022-11-19 10:33:51','White Shiny Heels','short_shirt_msh1010.jpg',190000,'Phong cách và sự quyến rũ ở mức tốt nhất',9,0,0,0),(17,5,1,'2022-11-19 10:33:52','Thrashers Hoodie','hoodie-2.png',190000,'Rất thoải mái, ấm áp và mát mẻ',11,0,0,0),(18,3,2,'2022-11-19 10:33:52','Black Ripped Jeans','jeanss.png',180000,'Rất tuyệt và phong cách',13,0,0,0),(19,5,3,'2022-11-19 10:33:51','Colorful Hoodie','hoodie-4.png',230000,'Rất tuyệt',12,0,0,0),(20,1,3,'2022-11-19 11:11:49','Black Polo Jacket','blue_jacket.jpg',310000,'Ấm áp và thoải mái',11,0,0,0),(21,5,3,'2022-11-19 10:33:52','Black Puma Hood','hoodie-3.png',788900,'Ấm áp và mát mẻ',10,0,0,0),(22,5,1,'2022-11-19 10:33:51','Black and White Hood','hoodie-1.png',210300,'Những người hâm mộ anime Cool Hood',9,0,0,0),(23,2,1,'2022-11-19 10:33:51','B&W Tee','women_shirt_grande.jpg',13000,'Rất tuyệt vời',8,0,0,0),(24,6,1,'2022-11-19 10:47:56','UV fabric hat MHA','hat_wonman.jpg',169000,'vải  chống nắng uv 98%, vải tricot UV 60% Cotton + 40% Poly. Sản phẩm có thể đội đi chơi, du lịch, trải nghiệm,.....',5,0,0,0),(25,6,2,'2022-11-19 10:34:30','Hat 3D MHA 1002','hat_3d.jpg',98000,'Nón vải tiện lợi và thời trang',2,0,0,0),(26,6,3,'2022-11-19 10:36:07','MINIONS Embroidered Hat','hat_fashion.jpg',190900,'Sản phẩm có thể đội đi chơi, du lịch, trải nghiệm,.....',1,0,0,0),(27,6,1,'2022-11-19 10:38:26','Khaki Hat 6 Points Top Silicon Logo','hat_logo_silicon.jpg',123000,'Sản phẩm form nón năng động, trẻ trung phù hợp với nhiều outfit.',2,0,0,0);
+INSERT INTO `products` VALUES (1,4,1,'2022-11-19 03:33:52','Men Terry Jogers','product1.jpg',145500,'Mẫu thời trang Thu - Đông đẹp nhất năm',10),
+                              (2,3,2,'2022-11-19 03:33:52','Wild Leg Denim','product2.jpg',177800,'Rất phong cách và dễ dàng',5),
+                              (3,2,2,'2022-11-19 03:33:51','Printed Days Tee','product3.jpg',80770,'Rất mát mẻ và thoải mái',20),
+                              (4,3,1,'2022-11-19 06:09:25','Men Stretch Skinny','product4.jpg',227700,'Phong cách và Mát mẻ',3),
+                              (6,2,3,'2022-11-19 03:33:51','BOYS TANK TOP','shirt_caro_msh1009.jpg',888976,'Rất hợp thời trang',21),
+                              (7,2,2,'2022-11-19 03:33:52','Black JUMPSUIT','product6.jpg',299909,'Rất thoải mái và mát mẻ',22),
+                              (9,1,3,'2022-11-19 04:11:49','Black Jacket','white_jacket.jpg',321100,'Rất ấm áp và thoải mái',24),
+                              (10,1,3,'2022-11-19 04:11:49','Red Parachute Jacket','yellow_jacket.jpg',23000,'Rất thoải mái và ấm áp',22),
+                              (11,2,2,'2022-11-19 03:33:51','Printed White Tee','short_shirt_msh1010.jpg',75000,'Rất thoải mái và ấm áp',11),
+                              (12,1,2,'2022-11-19 03:33:51','Brown Coat Type Jacket','productbig1.jpg',270000,'Thoải mái và ấm áp',5),
+                              (13,1,2,'2022-11-19 03:33:51','Pink Fluffy Jacket','productbig2.jpg',320000,'Rất thoải mái và ấm áp',6),
+                              (14,4,2,'2022-11-19 03:33:51','Black High Heels','productbig3.jpg',230000,'Rất phong cách và thoải mái',7),
+                              (15,1,1,'2022-11-19 03:33:51','Grey Royal Jacket','productbig4.jpg',350000,'Phong cách ấm áp và thoải mái',8),
+                              (16,4,2,'2022-11-19 03:33:51','White Shiny Heels','short_shirt_msh1010.jpg',190000,'Phong cách và sự quyến rũ ở mức tốt nhất',9),
+                              (17,5,1,'2022-11-19 03:33:52','Thrashers Hoodie','hoodie-2.png',190000,'Rất thoải mái, ấm áp và mát mẻ',11),
+                              (18,3,2,'2022-11-19 03:33:52','Black Ripped Jeans','jeanss.png',180000,'Rất tuyệt và phong cách',13),
+                              (19,5,3,'2022-11-19 03:33:51','Colorful Hoodie','hoodie-4.png',230000,'Rất tuyệt',12),
+                              (20,1,3,'2022-11-19 04:11:49','Black Polo Jacket','blue_jacket.jpg',310000,'Ấm áp và thoải mái',11),
+                              (21,5,3,'2022-11-19 03:33:52','Black Puma Hood','hoodie-3.png',788900,'Ấm áp và mát mẻ',10),
+                              (22,5,1,'2022-11-19 03:33:51','Black and White Hood','hoodie-1.png',210300,'Những người hâm mộ anime Cool Hood',9),
+                              (23,2,1,'2022-11-19 03:33:51','B&W Tee','women_shirt_grande.jpg',13000,'Rất tuyệt vời',8),
+                              (24,6,1,'2022-11-19 03:47:56','UV fabric hat MHA','hat_wonman.jpg',169000,'vải  chống nắng uv 98%, vải tricot UV 60% Cotton + 40% Poly. Sản phẩm có thể đội đi chơi, du lịch, trải nghiệm,.....',5),
+                              (25,6,2,'2022-11-19 03:34:30','Hat 3D MHA 1002','hat_3d.jpg',98000,'Nón vải tiện lợi và thời trang',2),
+                              (26,6,3,'2022-11-19 03:36:07','MINIONS Embroidered Hat','hat_fashion.jpg',190900,'Sản phẩm có thể đội đi chơi, du lịch, trải nghiệm,.....',1),
+                              (27,6,1,'2022-11-19 03:38:26','Khaki Hat 6 Points Top Silicon Logo','hat_logo_silicon.jpg',123000,'Sản phẩm form nón năng động, trẻ trung phù hợp với nhiều outfit.',2);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -286,14 +302,4 @@ LOCK TABLES `user_role` WRITE;
 INSERT INTO `user_role` VALUES (1,13,1),(2,13,2);
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-11-21 17:19:20
