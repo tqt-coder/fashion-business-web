4b880963e413bd33343768fcd1d7760d92bc5123
TUANPC\tranq <tranquoctuan31012001@gmail.com>
2022-11-22 08:58:09 +0700

    update cart for full product

--- src/main/java/com/project/fashionbusinesswebsite/service/ProductService.java
+++ src/main/java/com/project/fashionbusinesswebsite/service/ProductService.java
@@ -9,7 +9,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
+ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
